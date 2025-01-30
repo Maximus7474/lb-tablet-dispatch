9efc4627eb05003f2699aa01198030b6f09ad374
Maximus7474 <the.maximus.7474@gmail.com>
2025-01-30 21:13:13 +0100

    feat(fxmanifest): extra tags

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
+version '0.0.0'
+
+author 'Maximus7474'
+repository 'https://github.com/Maximus7474/lb-tablet-dispatch'
+
 dependancy 'lb-tablet'
 
 client_script 'client/*.lua'
