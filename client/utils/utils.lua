e6b22fb8f15f078e0792334baf666be290cd8637
Maximus7474 <the.maximus.7474@gmail.com>
2025-02-05 21:55:04 +0100

    feat(client/utils): ShouldNotify func

--- client/utils/utils.lua
+++ client/utils/utils.lua
@@ -12,6 +12,18 @@ function GetStreetAndAreaFromCoords(coords)
     return streetName, areaName
 end
 
+---Random check, should police be informed based on a tolerance value
+---@param tolerance number number between 0 and 100 inclusive
+---@return boolean notify did the random math stuff think it's time to call em ?
+function ShouldNotify(tolerance)
+    if type(tolerance) ~= "number" then return false end
+    if tolerance <= 0 then return false end
+    if tolerance >= 100 then return true end
+
+    tolerance = math.max(0, math.min(100, tolerance))
+    return math.random(0, 100) <= tolerance
+end
+
 ---Get if resource exists and is running
 ---@param resource string Resource to check
 ---@return boolean isrunning Is the resource running
